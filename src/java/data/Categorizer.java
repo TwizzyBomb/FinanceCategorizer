0e52ae55078985102f40d10f213f07834d27e246
User <User@Brian-PC>
2014-07-10 17:23:55 -0400

    Work on :  *Create properties objects that would be named various spending areas such as properties Food, properties Rent, which will contains the various expenditure strings as keys, and the amounts spent at each place as values.
     *Create a calculater which takes in the amounts you get paid, (this week and YTD)  from your pay stubs (eventually dynamic) and generates the percentages that you spend in each area. 
     
    7/10/14 - Got it to read in CSV files and use objects to get the exact data back that's necessary

---
+++ src/java/data/Categorizer.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package data;
+import java.io.*;
+import java.util.*;
+/**
+ *
+ * @author User
+ */
+public class Categorizer {
+
+    static String tempFilePath = "C:\\Users\\User\\Downloads\\csvdownload.csv";
+
+    //Eventually used to hold a lines worth of data from the CSV file
+    static String fileName;//used in Buffered reader conditional which builds arrays
+    //Creates Array list holding Spending Objects
+    static ArrayList<Spending> expenses = new ArrayList<Spending>();
+    //Holds the , separated values from fileName in order to plug them into the
+    //spending function as parameters
+    static String[] tempArray = new String[5];
+    
+    
+    public static Object RecentSpending() throws FileNotFoundException, IOException {
+    
+        //Java's way of reading files
+        FileReader fr = new FileReader(tempFilePath);
+        BufferedReader br = new BufferedReader(fr);
+        //separates the values, java wanted me to split declaration
+        StringTokenizer st = null;
+        int lineNumber = 0, tokenNumber = 0;
+        final String delimiter = ",";
+        //Sets string fileName to the line in the csv file, (to be delimited by commas later)
+        //and in the same step checks if this line is equal to null as the exit condition of the loop
+        for(lineNumber = 1; (fileName = br.readLine()) != null; lineNumber++){
+
+            System.out.println(fileName);
+            
+            //break comma separated line using ","
+            st = new StringTokenizer(fileName, delimiter);
+            while (st.hasMoreTokens()) {
+                    tokenNumber++;
+                    //use value to hold nextToken
+                    String value = st.nextToken();
+                    System.out.println("Line # " + lineNumber
+                            + ", Token # " + tokenNumber
+                            + ", Token : " + value);//value
+                            //sets value to temp array to properly populate object
+                            tempArray[tokenNumber] = value;
+                            expenses.add( new Spending(tempArray[1],tempArray[2],tempArray[3],tempArray[4]) );
+            }
+            //reset token number
+            tokenNumber = 0;
+        }
+        System.out.print("Expenses: " + expenses.get(7).getDescription());
+        return expenses;
+        //perhaps this could return as a key value pair list? of descriptions and amounts
+    }
+    
+    public static void main(String[] args) {
+    // TODO code application logic here
+        try{
+        //ArrayList<Spending> expense = RecentSpending();
+        RecentSpending();
+        //System.out.println(RecentSpending());
+        }catch(IOException i){
+            //do stuff
+        }
+        
+    }
+        
+}
+    
+
---
+++ src/java/data/Spending.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package data;
+
+/**
+ *
+ * @author User
+ */
+
+public class Spending {
+    private String date;
+    private String description;
+    private String amount;
+    private String balance;
+    
+    public Spending(String date, String description, String amount, String balance){
+    setAll(date, description, amount, balance);
+    
+    //this.date = date;
+    System.out.println("from spending object, date = " + date);
+    //this.description = description;
+    System.out.println("from spending object, description = " + description);
+    //this.amount = amount;
+    System.out.println("from spending object, amount = " + amount);
+    //this.balance = balance;
+    System.out.println("from spending object, balance = " + balance);
+    
+    }
+    
+    public void setAll(String da, String d, String a, String b){
+        setDate(da);
+        setDescription(d);
+        setAmount(a);
+        setBalance(b);
+    }
+    
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getBalance() {
+        return balance;
+    }
+
+    public void setBalance(String balance) {
+        this.balance = balance;
+    }
+    
+}
