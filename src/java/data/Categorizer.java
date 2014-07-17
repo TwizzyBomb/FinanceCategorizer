2b34cdc433c1572db77d154aa6ebe57049256523
User <User@Brian-PC>
2014-07-17 17:23:35 -0400

    Working on : Sorter function   Should need to use REGEX, just make sure it prints out the middle string consistently-      Now we can stack these strings and have categories for them.   Takes in description string, strips out the heading and trailing strings,   Compares middle string "Gulf Oil" and   and returns the category it goes into.
    Everything:
      Could make sql tables with each category and pull the amounts later out to do calculations.
      
     *Create properties objects that would be named various spending areas such as properties Food, properties Rent, which will contains the various expenditure strings as keys, and the amounts spent at each place as values.
     
     *Create a calculater which takes in the amounts you get paid, (this week and YTD)  from your pay stubs (eventually dynamic) and generates the percentages that you spend in each area. 
     
    7/17/14 - Regex working
    7/10/14 - Got it to read in CSV files and use objects to get the exact data back that's necessary
    7/11/14 - Got a swing form to display the data, better understanding of arrayLists
    7/16/14 - Got Regex working, keep working on getting that middle string separated

--- src/java/data/Categorizer.java
+++ src/java/data/Categorizer.java
@@ -63,6 +63,7 @@ public class Categorizer {
         //perhaps this could return as a key value pair list? of descriptions and amounts
     }
     
+    //Sorts Center String
     String[] eventuallySortable  = {"DDA PURCHASE", "VISA DDA PUR", "DDA WITHDRAW", "CHECK", "DEBIT"};
     String[] immediatelySortable = {"IBASE", "Online Xfer from", "Online Xfer to", "MAINTENANCE FEE", 
                                     "Debit", "ST TRF TRANSFER TO SAVINGS ACCT", "NONTD ATM FEE", "DDA DEPOSIT"};
@@ -71,49 +72,67 @@ public class Categorizer {
                                     "DDA PURCHASE 001        STOP   SHOP  637            STAMFORD      * CT",
                                     "VISA DDA PUR 423168     GULF OIL 91190990           FAIRFIELD     * CT",
                                     "VISA DDA PUR 423168    SOME OTHER STRING   FAIRFIELD"};
+    private static String expendature;
+    private static int indx2;
+    private static int indx;
     
     public static String Sorter(String fullDescription){
-        String spaces = "(\\s{3,})";//You should be able to get it to print out the string you need with regex dude.
-        Pattern pattern = Pattern.compile(spaces);
-        Matcher m = pattern.matcher(fullDescription);
+        String whiteSpaces = "(\\s{3,})";//You should be able to get it to print out the string you need with regex dude.
+        String nonWhiteSpaces = "(\\w{2,})";
+        Pattern wsRegex = Pattern.compile(whiteSpaces);
+        Pattern nwsRegex = Pattern.compile(nonWhiteSpaces);
+        Matcher m = wsRegex.matcher(fullDescription);
+        Matcher m2;
         String spaces3 = "   ";
-        int indx;
-        if(m.find()){
+        int i = 0;
+        String secondHalf;
+        
+        while(m.find()){
             indx = m.start();
-            System.out.println("Found REGEX @ " + indx);
+            //System.out.println("start: " + indx);//should be the first instance of 3 spaces after 1st iteration
+            //System.out.println("end: " + indx2);//should be the end of the previous white space
+            if(i==1){
+                expendature = fullDescription.substring(indx2, indx);
+                //secondHalf = fullDescription.substring(indx2);
+                //System.out.println("substring=" + secondHalf);
+                //m2 = nwsRegex.matcher(secondHalf);
+                //System.out.println(m2.start());
+                //expendature = fullDescription.substring(m2.start(), /*start of next one*/m.start());
+            }
+            indx2 = m.end();
+            //System.out.println("end: " + m.end());
+            i++;
+            //System.out.println("Found white spaces @ " + indx + " To " + indx2 + " i= " + i);
         }
-        //got index, now we have to count the number of spaces and turn that into the new index.
-        int spaceNumber;//loop through spaces until you get to the text again. you can use a regex or while!=" "
-        //for(int i=0; i<=spaceNumber; i++; ){
-        //    if(fullDescription.charAt(indx)===" "){       
-        //    }
-        //}
-        int index1 = fullDescription.indexOf(spaces);//Found it when you use "   " doesn't work with the regex
+        //got index, now we have to count the number of non-white space characters and get that new index
+        //expendature = fullDescription.substring(indx2,indx3);
+        System.out.println(expendature);
+        int index1 = fullDescription.indexOf(spaces3);
         System.out.println(index1);
         String temp = fullDescription.substring(index1);
         System.out.println(temp);
         int index2 = temp.indexOf(spaces3);
         System.out.println(index2);
-        String str = fullDescription.substring(index1, index2);
+        //String str = fullDescription.substring(index1, index2);
         //System.out.println(str);
-        return str;
+        return expendature;
     }
     
     public static void main(String[] args) {
     // TODO code application logic here
-        //try{
-        //Categorizer catClass = new Categorizer();
-        //ArrayList<Spending> arr = new ArrayList();
-        //arr = catClass.RecentSpending();
-        //String something = arr.get(4).getDescription();
-        //System.out.println("Something: " + something);
-        String localSomething = testStrings[4];
+        try{
+        Categorizer catClass = new Categorizer();
+        ArrayList<Spending> arr = new ArrayList();
+        arr = catClass.RecentSpending();
+        String something = arr.get(4).getDescription();
+        System.out.println("Something: " + something);
+        String localSomething = testStrings[2];
         System.out.println("localSomething: " + localSomething);
-        String something1 = Sorter(localSomething);
+        String something1 = Sorter(something);
         System.out.println("Sorted = " + something1);
-        //}catch(IOException i){
+        }catch(IOException i){
             //do stuff Was IO Exception
-        //}
+        }
         
     }
         
